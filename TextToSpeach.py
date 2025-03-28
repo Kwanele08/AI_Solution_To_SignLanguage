ca692d6004fc49f79b1f688dd73351cba3ee919e
mushabatiedwin <mushabatiedwin2@gmail.com>
2025-03-28 15:38:29 +0530

    Update Mistakes.py

--- TextToSpeach.py
+++ TextToSpeach.py
@@ -1,10 +1,1 @@
-import pyttsx3
-
-def TextToSpeach(text):
-    SpeachEngine = pyttsx3.init()
-    SpeachEngine.setProperty('rate', 150)
-    SpeachEngine.setProperty('volume', 1.0)
-
-    SpeachEngine.say(text)
-    SpeachEngine.runAndWait()
 
